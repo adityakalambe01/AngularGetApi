645220b2fdb1358c61fbb40a17d68803cdf5b87b
Aditya Kalambe <adityakalambe20@gmail.com>
2024-10-10 01:43:36 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'getApi';
+
+  imageList: any[] = [];
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http
+        .get('https://jsonplaceholder.typicode.com/photos')
+        .subscribe((response:any) =>{
+          this.imageList = response;
+        }, error =>{
+          console.log("Something went wrong");
+        });
+  }
+
 }
